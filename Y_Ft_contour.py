26814db70882620606c23d98e2a912f98d4eb0a8
Selim Harfouche <selim.harfouche@lau.edu>
2022-11-02 16:48:02 +0200

    renaming, cleanup & code update, problems with relative imports avoided by keeping everything within the same directory

---
+++ Y_Ft_contour.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+source_image="Images/0/hsf_0_00000.png"
+test_image="TEST/test.png"
+img = cv2.imread(source_image, cv2.IMREAD_UNCHANGED)
+
+#convert img to grey
+img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#set a thresh
+thresh = 100
+#get threshold image
+ret,thresh_img = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)
+#find contours
+contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+#create an empty image for contours
+img_contours = np.zeros(img.shape)
+# draw the contours on the empty image
+cv2.drawContours(img_contours, contours, -1, (0,255,0), 3)
+#save image
+cv2.imwrite(test_image,img_contours) 
+
+
+############
+img = cv2.imread(test_image,0)
+
+dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
+dft_shift = np.fft.fftshift(dft)
+
+magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
