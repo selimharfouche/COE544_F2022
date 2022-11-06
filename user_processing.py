842b4e506fc88dbf8ca6b1370d78dc1e808101d1
selimharfouche <57585478+selimharfouche@users.noreply.github.com>
2022-11-06 20:58:46 +0200

    added static method to process data

--- user_processing.py
+++ user_processing.py
@@ -3,22 +3,28 @@ from joblib import load
 import numpy as np
 
 class user_processing:
-    def __init__(self,learner="",features=[]):
+    def __init__(self,learner=""):
         self.learner = learner
-        self.features = features
-    def give_label(self):
+       
+
+    @staticmethod
+    def prepare_data(features=[]):
         # preparing the dataset using features selected by user
         # N.B.: WE ARE USING NUMERICAL FOR THE SPEED
-        p1 = data_prep(numerical = True, features_array=self.features)
+        p1 = data_prep( features_array=features)
         p1.prep_data()
 
         # preparing the image uploaded by the user
-        p1 = data_prep(numerical = True, features_array=self.features, uploaded=True)
+        p1 = data_prep(numerical = True, features_array=features, uploaded=True)
         p1.prep_data()
 
+
+    def give_label(self):
+
         if self.learner=="SVM":
+            print("SVM IS SELECTED")
             from A_learner_svm import SVM_class
-            svm = SVM_class(confusion_matrix=False,minimal_grid_search=False)
+            svm = SVM_class(confusion_matrix=False,minimal_grid_search=False,scaler="te")
             svm.train()
             estimator = load("best_estimators/SVM_BEST.joblib")
             X_test = load(open('data/X_test_uploaded.pkl', 'rb'))
@@ -35,7 +41,7 @@ class user_processing:
         
         if self.learner=="KNN":
             from A_learner_knn import KNN_class
-            knn = KNN_class()
+            knn = KNN_class(scaler="te")
             knn.train()
             estimator = load("best_estimators/KNN_BEST.joblib")
             X_test = load(open('data/X_test_uploaded.pkl', 'rb'))
