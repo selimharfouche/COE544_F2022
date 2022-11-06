b6162b26753d687d46dee378e620932057c19c2d
selimharfouche <57585478+selimharfouche@users.noreply.github.com>
2022-11-06 20:21:12 +0200

    added scaler option

--- A_learner_knn.py
+++ A_learner_knn.py
@@ -1,18 +1,18 @@
 from sklearn.neighbors import KNeighborsClassifier
 from pickle import load
-import pickle
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import SplineTransformer
+
 from sklearn.model_selection import train_test_split, GridSearchCV
 from joblib import load, dump
 
 
 
 class KNN_class():
-    def __init__(self) -> None:
-        pass
+    def __init__(self,scaler="ST") -> None:
+        self.scaler=scaler
 
     def train(self):
+        scaler=self.scaler
         X_train = load(open('data/X_train.pkl', 'rb'))
         X_test = load(open('data/X_test.pkl', 'rb'))
         Y_test = load(open('data/Y_test.pkl', 'rb'))
@@ -27,13 +27,13 @@ class KNN_class():
         # create an instance of grid search with the above parameters
         grid = GridSearchCV(knn_grid_tuned, parameters, cv=2, scoring='accuracy', return_train_score=True, verbose=10)
 
-        
-        
 
-   
-
-        # from sklearn.preprocessing import SplineTransformer
-        scaler = SplineTransformer()
+        if scaler=="ST":
+            from sklearn.preprocessing import SplineTransformer
+            self.scaler = SplineTransformer()
+        else:
+            from sklearn.preprocessing import StandardScaler
+            self.scaler = StandardScaler()
         X_train = scaler.fit_transform(X_train)
         #we must apply the scaling to the test set that we computed for the training set
         X_test = scaler.transform(X_test)
