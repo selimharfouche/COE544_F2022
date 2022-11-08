cd89ea49c017437d584ca999c5088d589ada5ff9
selimharfouche <57585478+selimharfouche@users.noreply.github.com>
2022-11-08 10:50:10 +0200

    test

--- machine_learning/scripts/user_processing.py
+++ machine_learning/scripts/user_processing.py
@@ -22,11 +22,17 @@ import numpy as np
 try:
     # running from Main.py
     from data_processing.data_prep import data_prep
-    from data_processing.characters_categories import mapping_labels
+    
 except:
     # running from flask, base.py
     from machine_learning.scripts.data_processing.data_prep import data_prep
+    
+
+try:
+    from data_processing.characters_categories import mapping_labels
+except:
     from machine_learning.scripts.data_processing.characters_categories import mapping_labels
+
 
 
 class user_processing:
@@ -111,7 +117,8 @@ class user_processing:
             print("PRDICIOT")
             print(model_prediction)
 
-            label = {i for i in mapping_labels if mapping_labels[i]==model_prediction[0]}
+            mapping_labels2 = mapping_labels
+            label = {i for i in mapping_labels2 if mapping_labels2[i]==model_prediction[0]}
             print("LABEL")
             print(label)
             expected_label=str(label.pop())[0]
@@ -141,7 +148,8 @@ class user_processing:
                 estimator = load("../machine_learning/best_estimators/ENSEMBLE.joblib")
                 
 
-            model_prediction = estimator.predict(X_test)
+            mapping_labels2 = mapping_labels
+            label = {i for i in mapping_labels2 if mapping_labels2[i]==model_prediction[0]}
             label = {i for i in mapping_labels if mapping_labels[i]==model_prediction[0]}
             expected_label=str(label.pop())[0]
             print ("Ensemble expected")
@@ -155,32 +163,40 @@ class user_processing:
 
             
         if self.learner=="PreTrained":
-            p1 = data_prep(numerical = True, features_array=["sobel_edge","HOG","pixel_intensity"], uploaded=True)
-            #p1 = data_prep(numerical = True, features_array=["HOG"], uploaded=True)
+            #p1 = data_prep(numerical = True, features_array=["sobel_edge","HOG","pixel_intensity"], uploaded=True)
+            p1 = data_prep(numerical = True, features_array=["HOG"], uploaded=True)
             p1.prep_data()
            
             try:
-                estimator = load("../machine_learning/models/svm_HOG1/svm_model3.joblib")
-                sc = load(open("../models/svm_HOG1/std_scaler3.pkl", "rb"))
+                estimator = load("../machine_learning/models/svm_HOG_Final/svm_model2.sav")
+                sc = load(open("../models/svm_HOG_Final/std_scaler2.bin", "rb"))
             except:
-                estimator = load("../machine_learning/models/svm_HOG1/svm_model3.joblib")
-                sc = load(open("../machine_learning/models/svm_HOG1/std_scaler3.pkl", "rb"))
+                estimator = load("../machine_learning/models/svm_HOG_Final/svm_model2.sav")
+                sc = load(open("../machine_learning/models/svm_HOG_Final/std_scaler2.bin", "rb"))
 
 
             sc1 = sc
         
 
-            #transformation for knn
+            #transformation for pre trained
             X_test = sc1.transform(X_test)
             
             model_prediction = estimator.predict(X_test)
-            label = {i for i in mapping_labels if mapping_labels[i]==model_prediction[0]}
+
+            print("MODEL PREDICTIO")
+            print(model_prediction)
+            model_prediction = sc1.inverse_transform(model_prediction)
+            print("MODEL PREDICTIO")
+            print(model_prediction)
+            
+            mapping_labels2 = mapping_labels
+            label = {i for i in mapping_labels2 if mapping_labels2[i]==model_prediction[0]}
             expected_label=str(label.pop())[0]
-            print ("Ensemble expected")
+            print ("Pre Trained")
             print(expected_label)
 
             probability=np.max(estimator.predict_proba(X_test))
-            print("Ensemble probability")
+            print("Pre Trained")
             print(probability)
 
             return [expected_label,probability]
